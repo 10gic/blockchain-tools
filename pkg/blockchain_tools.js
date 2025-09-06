21b297dcb6cc5a675ff4d0ce1aed72ad740570a4
10gic <2391796+10gic@users.noreply.github.com>
2025-09-06 17:20:20 +0800

    Supports automatic encoding detection

--- pkg/blockchain_tools.js
+++ pkg/blockchain_tools.js
@@ -239,6 +239,25 @@ export function encoding_convert(input, from_encoding, to_encoding) {
     }
 }
 
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+export function detect_encoding(input) {
+    let deferred2_0;
+    let deferred2_1;
+    try {
+        const ptr0 = passStringToWasm0(input, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        const len0 = WASM_VECTOR_LEN;
+        const ret = wasm.detect_encoding(ptr0, len0);
+        deferred2_0 = ret[0];
+        deferred2_1 = ret[1];
+        return getStringFromWasm0(ret[0], ret[1]);
+    } finally {
+        wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
+    }
+}
+
 async function __wbg_load(module, imports) {
     if (typeof Response === 'function' && module instanceof Response) {
         if (typeof WebAssembly.instantiateStreaming === 'function') {
